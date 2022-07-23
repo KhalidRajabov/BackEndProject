670ccb328b153c8188151e03dc7aa071581d9aa4
dbBurzum <khalidsr@code.edu.az>
2022-07-23 20:32:07 +0400

    started creating slider in adminpanel

--- BackEndProject/Models/Slider.cs
+++ BackEndProject/Models/Slider.cs
@@ -1,9 +1,14 @@
-﻿namespace BackEndProject.Models
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackEndProject.Models
 {
     public class Slider
     {
         public int Id { get; set; }
         public string ImageUrl { get; set; }
+        [NotMapped]
+        public IFormFile Photo { get; set; }
         public string Subtitle { get; set; }
         public string MainTitle { get; set; }
         public string Description { get; set; }
