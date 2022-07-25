0831b653c0ac96d2a2092ab27e7159f2cb807d85
dbBurzum <khalidsr@code.edu.az>
2022-07-25 21:44:26 +0400

    .

--- BackEndProject/Models/ProductImage.cs
+++ BackEndProject/Models/ProductImage.cs
@@ -1,9 +1,15 @@
-﻿namespace BackEndProject.Models
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackEndProject.Models
 {
     public class ProductImage
     {
         public int Id { get; set; }
         public string ImageUrl { get; set; }
+        [NotMapped]
+        public List<IFormFile> Photos { get; set; }
         public bool IsMain { get; set; }
 
         public int ProductId { get; set; }
