147e6d00f93d1f96afea4051edeb58a928925b65
dbBurzum <khalidsr@code.edu.az>
2022-08-01 17:32:24 +0400

    everything fixed on shop, home and detail page

---
+++ BackEndProject/Services/Category.cs
@@ -0,0 +1,27 @@
+﻿using BackEndProject.DAL;
+using BackEndProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BackEndProject.Services
+{
+    public class CategoryService : ICategory
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Category> Category()
+        {
+            List<Category> categories = _context.Categories
+                .Include(p => p.Children)
+                .Include(p => p.Products).Where(c => c.IsDeleted != true)
+                .ToList();
+            return categories;
+        }
+    }
+}
---
+++ BackEndProject/Services/ICategory.cs
@@ -0,0 +1,10 @@
+﻿using BackEndProject.Models;
+using System.Collections.Generic;
+
+namespace BackEndProject.Services
+{
+    public interface ICategory
+    {
+        public List<Category> Category();
+    }
+}
